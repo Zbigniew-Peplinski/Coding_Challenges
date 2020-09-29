0651aecb3e9de5e664c727028092d30d8357c296
Zbigniew-Peplinski <71725876+Zbigniew-Peplinski@users.noreply.github.com>
2020-09-29 15:08:57 +0200

    Add files via upload

---
+++ integer_to_roman.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+
+def inttoroman(n):
+    n = str(n)
+    ans = ''
+    dict = {'01':'M', '02':'', '03':'',
+            '11':'C', '12':'D', '13':'M',
+            '21':'X', '22':'L', '23':'C',
+            '31':'I', '32':'V', '33':'X'}
+    while len(n) < 4:
+        n = '0' + n
+    for i in range(4):
+        if n[i] == '0':
+            ans += ''
+        elif n[i] == '1':
+            ans += dict[str(i)+'1']
+        elif n[i] == '2':
+            ans += 2*dict[str(i)+'1']
+        elif n[i] == '3':
+            ans += 3*dict[str(i)+'1']
+        elif n[i] == '4':
+            ans += (dict[str(i)+'1'] + dict[str(i)+'2'])
+        elif n[i] == '5':
+            ans += dict[str(i)+'2']
+        elif n[i] == '6':
+            ans += (dict[str(i)+'2'] + dict[str(i)+'1'])
+        elif n[i] == '7':
+            ans += (dict[str(i)+'2'] + 2*dict[str(i)+'1'])
+        elif n[i] == '8':
+            ans += (dict[str(i)+'2'] + 3*dict[str(i)+'1'])
+        else:
+            ans += (dict[str(i)+'1'] + dict[str(i)+'3'])
+    return ans
